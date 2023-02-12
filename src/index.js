355a0f32a6cd8d3e0e04ba236c9a7273cacc5084
vasa3331 <vasiliymartinov@gmail.com>
2023-02-12 15:44:30 +0300

    feat:finish morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,41 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let strArr = expr.split('**********');
+    let strLetter = [];
+    for (let i = 0; i < strArr.length; i++) {
+      for (let j = 0; j < (strArr[i].length / 10); j++) {
+        strLetter.push(strArr[i].slice(j * 10, (j + 1) * 10));
+      }
+      if (i !== strArr.length - 1) {
+      strLetter.push(' ');
+      }
+    }
+   
+    let strLetterArr = strLetter.map(function(item) {
+      let result = '';
+      for (let i = 0; i < item.length / 2; i++) {
+        if (item.slice(i * 2, i * 2 + 2) == '10') {
+          result += '.';
+        } else if (item.slice(i * 2, i * 2 + 2) == '11') {
+          result += '-';
+        }
+      }
+      return item == ' ' ? item : result;      
+      });
+    let strFinish = strLetterArr.map(function(item) {
+        let result = '';
+        for (const [key, value] of Object.entries(MORSE_TABLE)) {
+            if(item == key) {
+                result = value;
+            } else if (item == ' ') {
+                result = ' ';
+                continue;
+            }
+          }
+        return result;      
+        });
+return strFinish.join('');
 }
 
 module.exports = {
